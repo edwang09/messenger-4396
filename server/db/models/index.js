a9ea3b8588bbb3dcce90544a41782dd76e8bc44c
Jianan Wang <edwang09@gmail.com>
2021-09-30 15:50:47 -0400

    database redesign

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,14 +1,16 @@
 const Conversation = require("./conversation");
 const User = require("./user");
+const db = require("../db");
 const Message = require("./message");
 
 // associations
+Conversation.belongsToMany(User, { as: "users", through: "ConversationUser" });
+User.belongsToMany(Conversation, { as: "conversations", through: "ConversationUser" });
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
 Message.belongsTo(Conversation, { as: "conversation" });
 Conversation.hasMany(Message);
+
+Message.belongsToMany(User, { as: "readers", through: "MessageRead" });
 
 module.exports = {
   User,
